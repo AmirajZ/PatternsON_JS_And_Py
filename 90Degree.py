a6f5c5fbe5ac2ec8387c9d5fb54cac79223e5b9f
Amirajsinh Zala <108674145+AmirajZ@users.noreply.github.com>
2022-11-15 14:24:47 +0530

    Create 90Degree.py

---
+++ 90Degree.py
@@ -0,0 +1,17 @@
+a = ["*","*","*","*","*"]
+b = " "
+for i in range(5):
+    c = "".join(a)
+    print(c)
+    a.insert(0," ")
+
+    
+    
+
+    
+
+
+# b = " "
+# b = b+a
+# print(b)
+# print(len(b))
